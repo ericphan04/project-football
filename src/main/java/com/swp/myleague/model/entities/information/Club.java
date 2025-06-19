657c459dde2cc86a21c78b61744e12f65fe0cdfa
Huynh Tan Trong <trong100103@gmail.com>
2025-06-19 14:41:37 +0700

    add entities
    infomation, match

---
+++ src/main/java/com/swp/myleague/model/entities/information/Club.java
@@ -0,0 +1,37 @@
+package com.swp.myleague.model.entities.information;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Club {
+    
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID clubId;
+
+    private String clubName;
+    private String clubLogoPath;
+    private String clubDescription;
+
+    @OneToMany(mappedBy = "club", fetch = FetchType.LAZY)
+    private List<Player> players;
+
+}
